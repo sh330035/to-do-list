1074c1838e413712e5282dfb3ef9a2de4c50b147
david3335 <sh330035@gmail.com>
2021-10-03 00:19:13 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,14 @@
+// module setting
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+// server listener
+app.listen(port, () => {
+  console.log(`Express runnning on port ${port}`)
+})
