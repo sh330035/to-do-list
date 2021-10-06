baabea14c39e49e51f62f5fec6be3a21191b7080
david3335 <sh330035@gmail.com>
2021-10-06 10:21:04 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.get('/', (req, res) => {
   todo
     .find()
     .lean() // 把資料轉換成單純的 JS object
+    .sort({ _id: 'asc' }) // 依據id升冪排列
     .then((todos) => res.render('index', { todos }))
     .catch((error) => console.log(error))
 })
